6e469dff085cc8e09f6bd3e3f75212153f2ef7b7
clu08 <clu08@wm.edu>
2024-10-15 12:18:09 -0400

    Made first query; need fixing

--- team1.py
+++ team1.py
@@ -1,2 +1,17 @@
 # Team1 best project
 # More comments for git testing
+
+import psycopg2
+
+conn = psycopg2.connect(dbname="team1")
+cur = conn.cursor()
+
+query = "select advisee.id, advisee.name, instructor.name from (select * from advisor, student where id = s_id) as advisee, instructor where instructor.id = i_id;"
+
+try:
+    cur.execute(query)
+    conn.commit()
+except psycopg2.Error as e:
+    print("Other Error")
+    print(e)
+    conn.rollback()
